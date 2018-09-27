a40601f39129441f830017f4da9e8a433b28cbef
EC2 Default User <ec2-user@ip-172-31-32-228.us-east-2.compute.internal>
2018-09-27 05:41:30 +0000

    completed all parts

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,8 @@
 class MoviesController < ApplicationController
+  helper_method :sort_column, :sort_direction
 
   def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
+    params.require(:movie).permit(:title, :rating, :description, :release_date, :sort, :direction)
   end
 
   def show
@@ -11,7 +12,14 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    if(!(Movie.column_names.include?(params[:sort]) &&%w[asc desc].include?(params[:direction]) && params[:ratings].present?))
+      redirect_to movies_path(sort: sort_column,direction: sort_direction, ratings: filter_ratings)
+    end
+    @movies = Movie.order('lower('+sort_column+')' + " " + sort_direction)
+    @movies = @movies.where(:rating => filter_ratings.keys)
+    @all_ratings = all_ratings
+    @selected_ratings = filter_ratings
+
   end
 
   def new
@@ -40,6 +48,33 @@ class MoviesController < ApplicationController
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
+  end
+  
+  def sort_column
+    session[:sort] =  Movie.column_names.include?(params[:sort]) ? params[:sort] : (Movie.column_names.include?(session[:sort]) ? session[:sort] : "title")
+    session[:sort]
+  end
+
+  def sort_direction
+    session[:direction] = %w[asc desc].include?(params[:direction]) ? params[:direction] : (%w[asc desc].include?(session[:direction]) ? session[:direction] : "asc")
+    session[:direction]
+  end
+  
+  def all_ratings
+    Movie.pluck(:rating).uniq
+  end
+  
+  def rating_with_keys
+    a = Hash.new
+    for rating in all_ratings
+     a[rating] = rating
+    end
+    a
+  end
+  
+  def filter_ratings
+   session[:ratings] =  params[:ratings].present? ? params[:ratings] : (session[:ratings].present? ? session[:ratings] : rating_with_keys)
+   session[:ratings]
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+    def sortable(column, title = nil)
+        direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+        link_to title, {:sort => column, :direction => direction}
+    end
+    def sort_class(column)
+        if(column == sort_column)
+           return 'hilite'
+        else
+           return nil
+        end
+    end
 end
